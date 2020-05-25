b9dd6ec3010a71ecc2f415250a2ac070b67db7c3
Brian Espina <espinabrian@gmail.com>
2020-05-25 21:07:37 +0800

    added rate calculator, added input validation

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
         }
     }
 
-    function time(){
+    function handleTime(){
 
         let timeToday = 0
 
@@ -23,18 +23,39 @@
             entryElement.textContent = val
             entriesElement.append(entryElement)
         }
+        const updateIncome = totals =>{
+            const rateInput = document.querySelector('#rateInput')
+            const income = (totals * rateInput.value) * 32
+            createTimeElement(income, 'rate', true)
+        }
         const updateTotals = accumulator => {
             const totals = document.getElementById('totals')
             totals.innerHTML = accumulator
+            updateIncome(accumulator)
         }
-
         const addTimeToday = () => {
             return function (event){
                 event.preventDefault()
                 createTimeElement(event.target[0].value, 'today')
-                timeToday += parseInt(event.target[0].value)
+                timeToday += validateInput(event.target[0].value)
                 updateTotals(timeToday)
+                input.value = ''
             }
+        }
+        function validateInput(val){
+            const splitVal = val.split(' ')
+            const array = splitVal.reduce((accumulator, current) => {
+                if(current.includes('h')){
+                    let hours = current.split('h').join('')
+                    accumulator.push(parseInt(hours))
+                }else{
+                    let minutes = current.split('m').join('')
+                    accumulator.push(parseInt(minutes) / 60)
+                }
+                return accumulator
+            }, [])
+            const output = array.reduce((accu, curr) => accu + curr, 0)
+            return output
         }
 
         const addTimeWeek = () => {
@@ -50,7 +71,7 @@
 
     }
 
-    const time = time2()
+    const time = handleTime()
     
     form.addEventListener('submit', time.addTimeToday())
     weekAdderButton.addEventListener('click', time.addTimeWeek())
